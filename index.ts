426c498549909e38ff6a7a789825952b59f0c129
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2022-05-15 13:47:35 -0300

    Apply lint to the project
    Include the script on package.json

--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
-import dotenv from 'dotenv'
-import express, {Request, Response} from 'express'
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT;
 
-app.get('/', (req: Request, resp: Response) => {
-    resp.send({ request: req.body, message: `Hello World at ${new Date()}` })
-})
+app.get("/", (req: Request, resp: Response) => {
+  resp.send({ request: req.body, message: `Hello World at ${new Date()}` });
+});
 
 app.listen(port, () => {
-    console.log("Server is up and running on port")
-})
+  console.log("Server is up and running on port");
+});
